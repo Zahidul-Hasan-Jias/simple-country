75d3c9ca39bf202394b8becc27370cb8cd58d928
Zahidul hasan jias <zahidulhasan887@gmail.com>
2026-03-27 21:58:47 +0600

    doing some change

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import { Suspense } from 'react'
+import './App.css'
+import Countries from './components/Countries/Countries'
+
+const countriesPromise = fetch('https://openapi.programming-hero.com/api/all')
+.then(res => res.json())
+
+function App() {
+
+  return (
+    <>
+      <Suspense fallback={<p>Wait for response</p>}>
+        <Countries countriesPromise={countriesPromise}></Countries>
+      </Suspense>
+    </>
+  )
+}
+
+export default App
